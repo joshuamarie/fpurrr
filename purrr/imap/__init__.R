2f1a7aab2772ab2d0c0ec77551e4242918f287e2
Joshua Marie <joshstat126@gmail.com>
2026-08-03 12:31:45 +0000

    Remove `df` type

--- purrr/imap/__init__.R
+++ purrr/imap/__init__.R
@@ -2,7 +2,7 @@ box::use(
     S7[new_class, class_function],
     purrr[imap_default = imap, raw = map_raw, vec = map_vec],
     ./types[lgl, int, dbl, chr],
-    ./df[df, dfr, dfc],
+    ./df[dfr, dfc],
 )
 
 imap_ns = new_class(
@@ -15,7 +15,6 @@ imap_ns = new_class(
         chr = class_function,
         raw = class_function,
         vec = class_function,
-        df = class_function,
         dfr = class_function,
         dfc = class_function
     )
@@ -30,7 +29,6 @@ call = imap_ns(
     chr = chr,
     raw = raw,
     vec = vec,
-    df = df,
     dfr = dfr,
     dfc = dfc
 )
--- purrr/imap/df.R
+++ purrr/imap/df.R
@@ -1,4 +1,4 @@
 #' @export
 box::use(
-    purrr[df = imap_df, dfc = imap_dfc, dfr = imap_dfr]
+    purrr[dfc = imap_dfc, dfr = imap_dfr]
 )
